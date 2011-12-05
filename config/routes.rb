e024b678423542203fb1c898e5ca39e3e2b28ca7
Jazmin Schroeder <jazminschroeder@gmail.com>
2011-12-04 21:22:28 -0600

    Add bootstrap

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Lab::Application.routes.draw do
   # The priority is based upon order of creation:
   # first created -> highest priority.
+  root :to => 'welcome#index'
   # Sample of regular route:
   #   match 'products/:id' => 'catalog#view'
   # Keep in mind you can assign values other than :controller and :action
